8d3c72924cd7c97cc76dbfb0cf1330c14d62b931
Toha <tohenk@yahoo.com>
2024-03-27 12:30:31 +0700

    Add Bootstrap DateTimePicker ID locale.
    Signed-off-by: Toha <tohenk@yahoo.com>

---
+++ bootstrap-datetimepicker-locale/id.js
@@ -0,0 +1,47 @@
+/*!
+  * Tempus Dominus v6.2.10 (https://getdatepicker.com/)
+  * Copyright 2013-2022 Jonathan Peterson
+  * Licensed under MIT (https://github.com/Eonasdan/tempus-dominus/blob/master/LICENSE)
+  */
+(function(g,f){typeof exports==='object'&&typeof module!=='undefined'?f(exports):typeof define==='function'&&define.amd?define(['exports'],f):(g=typeof globalThis!=='undefined'?globalThis:g||self,f((g.tempusDominus=g.tempusDominus||{},g.tempusDominus.locales=g.tempusDominus.locales||{},g.tempusDominus.locales.id={})));})(this,(function(exports){'use strict';const name = 'id';
+const localization = {
+    today: 'Hari ini',
+    clear: 'Bersihkan',
+    close: 'Tutup',
+    selectMonth: 'Pilih bulan',
+    previousMonth: 'Bulan sebelumnya',
+    nextMonth: 'Bulan selanjutnya',
+    selectYear: 'Pilih tahun',
+    previousYear: 'Tahun sebelumnya',
+    nextYear: 'Tahun selanjutnya',
+    selectDecade: 'Pilih dekade',
+    previousDecade: 'Dekade sebelumnya',
+    nextDecade: 'Dekade selanjutnya',
+    previousCentury: 'Abad sebelumnya',
+    nextCentury: 'Abad selanjutnya',
+    pickHour: "Pilih jam",
+    incrementHour: "Tambah jam",
+    decrementHour: "Kurangi jam",
+    pickMinute: 'Pilih menit',
+    incrementMinute: 'Tambah menit',
+    decrementMinute: 'Kurangi menit',
+    pickSecond: 'Pilih detik',
+    incrementSecond: 'Tambah detik',
+    decrementSecond: 'Kurangi detik',
+    toggleMeridiem: 'Ubah AM-PM',
+    selectTime: "Pilih waktu",
+    selectDate: 'Pilih tanggal',
+    dayViewHeaderFormat: { month: 'long', year: '2-digit' },
+    locale: 'id',
+    startOfTheWeek: 1,
+    dateFormats: {
+        LT: 'HH:mm',
+        LTS: 'HH:mm:ss',
+        L: 'dd/MM/yyyy',
+        LL: 'dd MMMM yyyy',
+        LLL: 'dd MMMM yyyy HH:mm',
+        LLLL: 'dddd dd MMMM yyyy HH:mm',
+    },
+    ordinal: (n) => n,
+    format: 'L LT',
+};exports.localization=localization;exports.name=name;Object.defineProperty(exports,'__esModule',{value:true});}));
